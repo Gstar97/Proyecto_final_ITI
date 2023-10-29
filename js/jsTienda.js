03fe6e668a416f6646c3f7d931ee14adfd58dabf
eros <eroschico3452@gmail.com>
2023-10-29 20:44:18 -0300

    Arreglo de administrador y tienda .js 	-Se volvio todo jquery 	-se arreglo el filtro

--- js/jsTienda.js
+++ js/jsTienda.js
@@ -1,5 +1,19 @@
 window.addEventListener("load", inicio);
 
+$("#btnAbrirModal").click(abrirModalCreacion);
+$("#cerrarFondoCreacion").click(cerrarFondoCreacion);
+$("#btnCrearMenu").click(crearVianda);
+$("#btnBuscarTienda").click(filtroDieta)
+$("#btnCarrito").click(abrirModalStock)
+$("#cerrarFondoStock").click(cerrarFondoStock);
+
+let modalBase = $("#modalBase");
+let modalStock = $("#modalStock");
+let fondoModal = $("#fondoModal");
+$(modalStock).css("display", "none");
+$(modalBase).css("display", "none");
+$(fondoModal).css("background-color", "transparent");
+
 function inicio(){
     let viandas = [
         {imagen: "imagenes/viandas/alubias_blancas.jpg", nombre: "Anchoas con pure", precio: 190.000, dieta: "celiacos"},
@@ -10,7 +24,7 @@ function inicio(){
     ];
     for (let i = 0; i < viandas.length; i++) {
         $("#zonaMenu").append(`
-        <section class="baseMenu col-8 col-sm-6 col-md-3 col-lg-2 mx-1 mx-md-3 mx-lg-3 py-3 mt-3 justify-content-center" class="dieta,`+viandas[i].dieta+`"> 
+        <section class="`+viandas[i].dieta+` baseMenu col-8 col-sm-6 col-md-3 col-lg-2 mx-1 mx-md-3 mx-lg-3 py-3 mt-3 justify-content-center"> 
             <img class="w-100 mb-2" src="`+viandas[i].imagen+`">
             <h4 class="text-center">`+viandas[i].nombre+`</h4>
             <h5 class="text-center">`+viandas[i].precio+`U$</h5>
@@ -27,47 +41,33 @@ function inicio(){
     
 }
 
-$("#btnAbrirModal").click(abrirModalCreacion);
-$("#cerrarFondoCreacion").click(cerrarFondoCreacion);
-$("#btnCrearMenu").click(crearVianda);
-$("#btnBuscarTienda").click(filtroDieta)
-$("#btnCarrito").click(abrirModalStock)
-$("#cerrarFondoStock").click(cerrarFondoStock);
-
-let modalBase = document.getElementById("modalBase");
-let modalStock = document.getElementById("modalStock");
-let fondoModal = document.getElementById("fondoModal");
-modalStock.style.display = "none";
-modalBase.style.display = "none";
-fondoModal.style.backgroundColor = "transparent"
-
 // Click para abrir el modal Stock
 function abrirModalStock(){
-    modalStock.style.display = "block";
-    modalBase.style.display = "none";
-    fondoModal.style.zIndex = "9"
-    fondoModal.style.backgroundColor = "rgba(0, 0, 0, 0.8)"
+    $(modalStock).css("display", "block");
+    $(modalBase).css("display", "none");
+    $(fondoModal).css("background-color", "rgba(0, 0, 0, 0.8)");
+    $(fondoModal).css("z-index", "9");
 }
 // Clic sobre <span> para cerrar el modal Stock
 function cerrarFondoStock(){
-    modalStock.style.display = "none";
-    fondoModal.style.zIndex = "-1"
-    fondoModal.style.backgroundColor = "transparent"
+    $(modalStock).css("display", "none");
+    $(modalBase).css("display", "none");
+    $(fondoModal).css("background-color", "transparent");
+    $(fondoModal).css("z-index", "-1");
 }
 // Clic para abrir el modal Creacion de menus
 function abrirModalCreacion(){
-    modalStock.style.display = "none";
-    modalBase.style.display = "block";
-    
-    fondoModal.style.zIndex = "9"
-    fondoModal.style.backgroundColor = "rgba(0, 0, 0, 0.8)"
+    $(modalStock).css("display", "none");
+    $(modalBase).css("display", "block");
+    $(fondoModal).css("background-color", "rgba(0, 0, 0, 0.8)");
+    $(fondoModal).css("z-index", "9");
 }
 // Clic sobre <span> para cerrar el modal creacion de menus
 function cerrarFondoCreacion(){
-    modalStock.style.display = "none";
-    modalBase.style.display = "none";
-    fondoModal.style.zIndex = "-1"
-    fondoModal.style.backgroundColor = "transparent"
+    $(modalStock).css("display", "none");
+    $(modalBase).css("display", "none");
+    $(fondoModal).css("background-color", "transparent");
+    $(fondoModal).css("z-index", "-1");
 }
 //Creacion de las viandas
 function crearVianda(){
@@ -92,55 +92,50 @@ function crearVianda(){
     }
 }
 
-
-//
 /*filtro de Dietas*/
 function filtroDieta() {
     let selectDietas = ($("#selectDietas").val());
     switch (selectDietas) {
         case "todo_dietas":
-            document.querySelectorAll("celiacos").css("display", "block");
-            document.querySelectorAll("vegana").css("display", "block");
-            document.querySelectorAll("vegetariana").css("display", "block");
-            document.querySelectorAll("ovovegetaria").css("display", "block");
-            document.querySelectorAll("ovolacteovegetariana").css("display", "block");
+            $(".celiacos").css("display", "block");
+            $(".vegana").css("display", "block");
+            $(".vegetariana").css("display", "block");
+            $(".ovovegetaria").css("display", "block");
+            $(".ovolacteovegetariana").css("display", "block");
         break;
         case "celiacos":
-            $("celiacos").css("display", "block");
-            $("vegana").css("display", "none");
-            $("vegetariana").css("display", "none");
-            $("ovovegetaria").css("display", "none");
-            $("ovolacteovegetariana").css("display", "none");
-            
+            $(".celiacos").css("display", "block");
+            $(".vegana").css("display", "none");
+            $(".vegetariana").css("display", "none");
+            $(".ovovegetaria").css("display", "none");
+            $(".ovolacteovegetariana").css("display", "none");
         break;
         case "vegana":
-            
-            document.querySelectorAll("celiacos").css("display", "none");
-            document.querySelectorAll("vegana").css("display", "block");
-            document.querySelectorAll("vegetariana").css("display", "none");
-            document.querySelectorAll("ovovegetaria").css("display", "none");
-            document.querySelectorAll("ovolacteovegetariana").css("display", "none");
+            $(".celiacos").css("display", "none");
+            $(".vegana").css("display", "block");
+            $(".vegetariana").css("display", "none");
+            $(".ovovegetaria").css("display", "none");
+            $(".ovolacteovegetariana").css("display", "none");
         break;
         case "vegetariana":
-            document.querySelectorAll("celiacos").css("display", "none");
-            document.querySelectorAll("vegana").css("display", "none");
-            document.querySelectorAll("vegetariana").css("display", "block");
-            document.querySelectorAll("ovovegetaria").css("display", "none");
-            document.querySelectorAll("ovolacteovegetariana").css("display", "none");
+            $(".celiacos").css("display", "none");
+            $(".vegana").css("display", "none");
+            $(".vegetariana").css("display", "block");
+            $(".ovovegetaria").css("display", "none");
+            $(".ovolacteovegetariana").css("display", "none");
         break;
         case "ovovegetaria":
-            document.querySelectorAll("celiacos").css("display", "none");
-            document.querySelectorAll("vegana").css("display", "none");
-            document.querySelectorAll("vegetariana").css("display", "none");
-            document.querySelectorAll("ovovegetaria").css("display", "block");
-            document.querySelectorAll("ovolacteovegetariana").css("display", "none");
-        break;
+            $(".celiacos").css("display", "none");
+            $(".vegana").css("display", "none");
+            $(".vegetariana").css("display", "none");
+            $(".ovovegetaria").css("display", "block");
+            $(".ovolacteovegetariana").css("display", "none");
         case "ovolacteovegetariana":
-            document.querySelectorAll("celiacos").css("display", "none");
-            document.querySelectorAll("vegana").css("display", "none");
-            document.querySelectorAll("vegetariana").css("display", "none");
-            document.querySelectorAll("ovovegetaria").css("display", "none");
-            document.querySelectorAll("ovolacteovegetariana").css("display", "block");
+            $(".celiacos").css("display", "none");
+            $(".vegana").css("display", "none");
+            $(".vegetariana").css("display", "none");
+            $(".ovovegetaria").css("display", "none");
+            $(".ovolacteovegetariana").css("display", "block");
         break;
     }
 }
