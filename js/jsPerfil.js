adb275f53d2212dcc7bae34de457b15e02d5ff85
Santiago <lucasantiago1212@gmail.com>
2023-11-10 21:10:55 -0300

    *Se arreglo bug en el perfil

--- js/jsPerfil.js
+++ js/jsPerfil.js
@@ -50,6 +50,46 @@ function inicio(){
             <P>Puerta: `+cliente.puerta+`</P>
             `);
         }
-    })
-   
+    })  
 }
+/*dsaparecer modal*/
+$("#modal").css("display", "none");
+$("#fondoModal").css("background-color", "transparent");
+//si hacemos click a carrito.
+$("#linkCarrito").click(carrito)
+function carrito(){
+  if(sessionStorage.getItem('usuario')){
+    let datos = sessionStorage.getItem('usuario');
+    let usuario = JSON.parse(datos);
+    if(usuario.autorizado == "true"){
+      window.location.href = "carrito.html"
+    }else if (usuario.autorizado == "false"){
+        $("#modal").css("display", "block");
+        $("#fondoModal").css("background-color", "rgba(0, 0, 0, 0.8)");
+        $("#fondoModal").css("z-index", "9");
+        $("#mensajeAdvertencia").html("Tu cuenta a un no esta Autorizada para acceder a esta funcionalidad")
+    }
+  }
+}
+//si hacemos click a tienda.
+$("#linkTienda").click(tienda)
+function tienda(){
+  if(sessionStorage.getItem('usuario')){
+    let datos = sessionStorage.getItem('usuario');
+    let usuario = JSON.parse(datos);
+    if(usuario.autorizado == "true"){
+      window.location.href = "tienda.html";
+    }else if (usuario.autorizado == "false"){
+      $("#modal").css("display", "block");
+      $("#fondoModal").css("background-color", "rgba(0, 0, 0, 0.8)");
+      $("#fondoModal").css("z-index", "9");
+      $("#mensajeAdvertencia").html("Tu cuenta a un no esta Autorizada para acceder a esta funcionalidad")
+    } 
+  }
+}
+//Para cerrar el modal
+$("#cerrarModal").click(function(){
+  $("#modal").css("display", "none");
+  $("#fondoModal").css("background-color", "transparent");
+  $("#fondoModal").css("z-index", "-1");
+});
